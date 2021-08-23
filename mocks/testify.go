ad53c653d75ffc1b8c03f616cdcf2ea8e2fbc32e
Xavier Vello <xavier@bigblue.io>
2021-08-23 15:53:48 +0200

    Add OnExecCommand test helper

--- mocks/testify.go
+++ mocks/testify.go
@@ -1,6 +1,7 @@
 package mocks
 
 import (
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,4 +21,31 @@ func (_m *Owl) ExpectAssertFailure(t *testing.T, contains string) {
 func (_m *Owl) ExpectRequireFailure(t *testing.T, contains string) {
 	_m.ExpectAssertFailure(t, contains)
 	_m.On("FailNow").Panic("FailNow called by subcommand")
+}
+
+type ExecCommandCall struct {
+	call *mock.Call
+	name string
+	arg  []string
+}
+
+func (_m *Owl) OnExecCommand(name string, arg ...string) *ExecCommandCall {
+	var parts []interface{}
+	parts = append(parts, name)
+	for _, a := range arg {
+		parts = append(parts, a)
+	}
+	return &ExecCommandCall{
+		call: _m.On("ExecCommand", parts...),
+		name: name,
+		arg:  arg,
+	}
+}
+
+func (c *ExecCommandCall) Run() *mock.Call {
+	return c.call.Return(exec.Command(c.name, c.arg...))
+}
+
+func (c *ExecCommandCall) ExecBash(shell string) *mock.Call {
+	return c.call.Return(exec.Command("bash", "-c", shell))
 }
--- must/must_test.go
+++ must/must_test.go
@@ -1,7 +1,6 @@
 package must
 
 import (
-	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,21 +9,26 @@ import (
 
 func TestExec_Simple(t *testing.T) {
 	mowl := new(mocks.Owl)
-	mowl.On("ExecCommand", "echo", "one", "two").
-		Return(exec.Command("echo", "one", "two")).Once()
+	mowl.OnExecCommand("echo", "one", "two").Run().Once()
 	assert.Equal(t, "one two", Exec(mowl, "echo", "one", "two"))
 }
 
 func TestExec_SplitArgs(t *testing.T) {
 	mowl := new(mocks.Owl)
-	mowl.On("ExecCommand", "echo", "a", "b", "c").
-		Return(exec.Command("echo", "a", "b", "c")).Once()
+	mowl.OnExecCommand("echo", "a", "b", "c").Run().Once()
 	assert.Equal(t, "a b c", Exec(mowl, "echo a b c"))
 }
 
 func TestExec_Err(t *testing.T) {
 	mowl := new(mocks.Owl)
-	mowl.On("ExecCommand", "false").Return(exec.Command("false")).Once()
+	mowl.OnExecCommand("false").Run().Once()
 	mowl.ExpectRequireFailure(t, "exit status 1")
 	assert.Panics(t, func() { Exec(mowl, "false") })
+}
+
+func TestExec_ErrFromBash(t *testing.T) {
+	mowl := new(mocks.Owl)
+	mowl.OnExecCommand("true").ExecBash("echo mocked; false").Once()
+	mowl.ExpectRequireFailure(t, "mocked")
+	assert.Panics(t, func() { Exec(mowl, "true") })
 }
